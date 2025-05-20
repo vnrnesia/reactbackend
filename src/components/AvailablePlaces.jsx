20ecc6cd1223da9c699c6e79a12bf911fbef5d48
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-05-20 13:53:12 +0300

    Loader added

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,25 +1,30 @@
-import { useState, useEffect } from 'react'; 
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
-const places = localStorage.getItem('places');
+const places = localStorage.getItem("places");
 
-export default  function AvailablePlaces({ onSelectPlace }) {
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetchingData(false);
+    }
 
-  fetch('http://localhost:3000/places').then((response) => {
-    return response.json()
-
-
-  }).then((resData) => {
-    setAvailablePlaces(resData.places)
-  }) ; 
+    fetchPlaces();
+  }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={true}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
